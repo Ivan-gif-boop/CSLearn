45237b6c1fccb038f248483500193101fd4e827b
Ivan-gif-boop <Ivan.wachira@strathmore.edu>
2025-11-10 18:41:33 +0300

    Erased backendapplication.java

--- backend/backend/src/main/java/com/cslearn/BackendApplication.java
+++ backend/backend/src/main/java/com/cslearn/BackendApplication.java
@@ -1,13 +1,1 @@
-package com.cslearn;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class BackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
-}
